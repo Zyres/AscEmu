cec7cdf0312c5085806ae8fab2ba29b4cb5da8fa
Zyres <mr@eichbuehlerhof.de>
2021-02-26 18:47:41 +0100

    Add missing include

--- src/shared/AENetwork/AENetworkCommon.hpp
+++ src/shared/AENetwork/AENetworkCommon.hpp
@@ -13,6 +13,7 @@ This file is released under the MIT license. See README-MIT for more information
 #include <vector>
 #include <cstdint>
 #include <algorithm>
+#include <mutex>
 
 #define ASIO_STANDALONE
 #include <asio.hpp>
