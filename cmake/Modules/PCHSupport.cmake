13ed3ae78a95c0fe977f8029647741dc3cef5e42
Zyres <mr@eichbuehlerhof.de>
2021-06-30 14:42:50 +0200

    pch

--- cmake/Modules/PCHSupport.cmake
+++ cmake/Modules/PCHSupport.cmake
@@ -1,3 +1,5 @@
+include(cotire)
+
 function(GEN_CXX_PCH TARGET_HEADER_LIST PCH_HEADERS)
     foreach(TARGET_HEADER ${TARGET_HEADER_LIST})
       target_precompile_headers(${TARGET_HEADER} PRIVATE ${PCH_HEADERS})
