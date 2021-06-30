6ff852c1f56d4b917ddc5fcb5d4acd8e681cd988
Zyres <mr@eichbuehlerhof.de>
2021-06-30 21:41:35 +0200

    mess

--- cmake/Modules/PCHSupport.cmake
+++ cmake/Modules/PCHSupport.cmake
@@ -1,16 +1,28 @@
-include(cotire)
+if (CMAKE_VERSION VERSION_LESS "3.16.0")
+	if (MSVC)
+	  set(COTIRE_PCH_MEMORY_SCALING_FACTOR 500)
+	endif()
 
-function(GEN_CXX_PCH TARGET_LIST PCH_HEADERS)
-  # iterate through targets
-  foreach(TARGET_HEADER ${TARGET_LIST})
-    # unity builds disabled
-    set_target_properties(${TARGET_HEADER} PROPERTIES COTIRE_ADD_UNITY_BUILD OFF)
+	include(cotire)
 
-    # prefix header
-    set_target_properties(${TARGET_HEADER} PROPERTIES COTIRE_CXX_PREFIX_HEADER_INIT ${PCH_HEADERS})
-	
-	# Workaround for cotire bug: https://github.com/sakra/cotire/issues/138
-    set_property(TARGET ${TARGET_HEADER} PROPERTY CXX_STANDARD 17)
-  endforeach()
-  cotire(${TARGET_LIST})
-endfunction(GEN_CXX_PCH)
+	function(GEN_CXX_PCH TARGET_LIST PCH_HEADERS)
+	  # iterate through targets
+	  foreach(TARGET_HEADER ${TARGET_LIST})
+		# unity builds disabled
+		set_target_properties(${TARGET_HEADER} PROPERTIES COTIRE_ADD_UNITY_BUILD OFF)
+
+		# prefix header
+		set_target_properties(${TARGET_HEADER} PROPERTIES COTIRE_CXX_PREFIX_HEADER_INIT ${PCH_HEADERS})
+		
+		# Workaround for cotire bug: https://github.com/sakra/cotire/issues/138
+		set_property(TARGET ${TARGET_HEADER} PROPERTY CXX_STANDARD 17)
+	  endforeach()
+	  cotire(${TARGET_LIST})
+	endfunction(GEN_CXX_PCH)
+else()
+	function(GEN_CXX_PCH TARGET_LIST PCH_HEADERS)
+		foreach(TARGET_HEADER ${TARGET_LIST})
+			target_precompile_headers(${TARGET_HEADER} PRIVATE ${PCH_HEADERS})
+		endforeach()
+	endfunction(GEN_CXX_PCH)
+endif()
