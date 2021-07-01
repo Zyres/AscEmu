afbbc832648bf2b20cc617cede8fea43f51843bb
Zyres <mr@eichbuehlerhof.de>
2021-07-01 17:45:16 +0200

    Update ExtraScriptsPCH.hpp

--- src/scripts/ExtraScripts/PCH/ExtraScriptsPCH.hpp
+++ src/scripts/ExtraScripts/PCH/ExtraScriptsPCH.hpp
@@ -4,9 +4,3 @@ This file is released under the MIT license. See README-MIT for more information
 */
 
 #include "Setup.h"
-#include "Server/Script/CreatureAIScript.h"
-#include "Server/Script/ScriptSetup.h"
-#include "Server/Script/ScriptMgr.h"
-#include "Server/ServerState.h"
-
-#include "Units/Creatures/Creature.h"
